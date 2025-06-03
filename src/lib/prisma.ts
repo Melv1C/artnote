ad152fa15ea711fe42171891a720330dc8b2d3c4
Melv1C <claes.melvyn@gmail.com>
2025-06-03 12:58:36 +0200

    feat: update PrismaClient import and enhance client initialization with acceleration support

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,22 @@
-import { PrismaClient } from '@/generated/prisma';
+import { PrismaClient } from '@/generated/prisma/edge';
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma: PrismaClient;
 };
+
+const getPrismaClient = () => {
+  const client = new PrismaClient();
+
+  if (process.env.NODE_ENV === 'production') {
+    return client.$extends(withAccelerate());
+  }
+
+  return client;
+}
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient();
+  getPrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
