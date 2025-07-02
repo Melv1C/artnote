ee57474291ad800a7d8c0f5368c4be44a87eb2a4
Melvyn Claes <claes.melvyn@gmail.com>
2025-07-02 10:18:32 +0200

    feat(profile): add avatar upload

--- src/features/auth/components/index.ts
+++ src/features/auth/components/index.ts
@@ -3,3 +3,4 @@ export { AuthDialog } from './auth-dialog';
 export { SignInForm } from './sign-in-form';
 export { SignUpForm } from './sign-up-form';
 export { UserButton } from './user-button';
+export { AvatarUpload } from './avatar-upload';
