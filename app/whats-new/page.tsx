a39314b18edb15f315fc004bd50bb28063bdafdb
Melvyn Claes <claes.melvyn@gmail.com>
2025-10-09 21:00:04 +0200

    feat: add dynamic export for WhatsNewPage component

--- app/whats-new/page.tsx
+++ app/whats-new/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { ArtworkCard, ArtworksListSkeleton } from '@/features/artworks';
 import { getLatestPublishedArtworks } from '@/features/artworks/actions';
 import { Suspense } from 'react';
